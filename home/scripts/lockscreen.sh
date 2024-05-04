3c8353efcccb31082e7d6dc2085db6924cf3ff6b
Joey Li <ljyhd2024@gmail.com>
2024-05-03 23:25:37 -0600

    optimze swaylock parameters for blue light filter

--- home/scripts/lockscreen.sh
+++ home/scripts/lockscreen.sh
@@ -7,18 +7,17 @@ fi
 
 swaylock \
     -f \
+    --image ~/.wallpaper/wallpaper \
     --font "Hack Nerd Font Mono" \
-    --screenshots \
     --clock \
     --indicator \
     --indicator-radius 150 \
     --indicator-thickness 7 \
-    --effect-blur 17x5 \
     --ring-color bb00cc \
     --key-hl-color 880033 \
     --line-color 00000000 \
     --inside-color 00000088 \
     --separator-color 00000000 \
     --grace 2 \
-    --fade-in 0.2 \
-    # --image ~/.wallpaper/wallpaper \
+    --fade-in 0.0 \
+    # --effect-blur 17x1 \
